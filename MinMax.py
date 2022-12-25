fe8b083edf9eaae0cb37f573598442a7b6fec13e
Dipesh Yadav <dipeshyadav4444@gmail.com>
2022-12-25 07:59:28 +0530

    "MinMax added"

---
+++ MinMax.py
@@ -0,0 +1,11 @@
+l = [1, 2, 44, 5, 10, 0, -2]
+
+print('Minimum in the list is :', min(l))
+print('Maximum in the list is :', max(l))
+
+num1 = int(input('Enter first number : '))
+num2 = int(input('Enter second number : '))
+num3 = int(input('Enter third number : '))
+
+print('Minimum in', num1, ',', num2, 'and', num3, 'is', min(num1,num2,num3))
+print('Maximum in', num1, ',', num2, 'and', num3, 'is', max(num1,num2,num3))
