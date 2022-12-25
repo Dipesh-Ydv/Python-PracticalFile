ee7b98c409a53b5064e4a70cb347a2f985c70029
Dipesh Yadav <dipeshyadav4444@gmail.com>
2022-12-25 15:17:55 +0530

    NoConversion added

---
+++ NoConversion.py
@@ -0,0 +1,21 @@
+num = 213
+# It will Decimal into Binary number system
+print(bin(num))
+# It will Decimal into Hexa-decimal number system
+print(hex(num))
+# It will Decimal into Octal number system
+print(oct(num))
+# It will convert Binary to Decimal number system
+print(int(0b10))
+# It will convert Hexa-decimal to Decimal number system
+print(int(0xdd4))
+# It will convert Octal to Decimal number system
+print(int(0o223))
+# It will convert Hexa-decimal to Octal number system
+print(oct(0xda4))
+# It will convert Binary to Octal number system
+print(oct(0b100))
+# It will convert Binary to Hexa-decimal number system
+print(hex(0b1001))
+# It will convert Octal to Hexa-decimal number system
+print(hex(0o23))
