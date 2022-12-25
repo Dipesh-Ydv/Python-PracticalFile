99ad53d7bc6fe9f5b5e60ea6b50a98953f6df6fe
Dipesh Yadav <dipeshyadav4444@gmail.com>
2022-12-25 10:05:27 +0530

    Exponentiation added

---
+++ Exponentiation.py
@@ -0,0 +1,12 @@
+import math as M
+
+def expo(base, power):
+
+    return base ** power
+
+base = int(input('Enter the number :'))
+power = int(input('Enter the power :'))
+print(base, 'power', power, 'is equal to : ', expo(base, power))
+
+# Finding using math modul
+print(base, 'power', power, 'is equal to : ', M.pow(base, power))
