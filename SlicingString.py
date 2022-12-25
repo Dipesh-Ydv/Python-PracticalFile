e8a92096336c6db9cf3efa6ff40c00a7d1641558
Dipesh Yadav <dipeshyadav4444@gmail.com>
2022-12-25 08:28:22 +0530

    StringSlicing updated

--- SlicingString.py
+++ SlicingString.py
@@ -1,7 +1,16 @@
 name = 'Dipesh Yadav'
 
-print(name)     # It will print the whole string
-print(name[1:8])    # It will print from index: 1 to 7
-print(name[::2])    # It will print the whole string by skipping 1 letter
-print(name[-1:-13:-1])      # It will print name in reverse
-print(name[12::-1])     # It will also print in reverse
+# It will print the whole string
+print(name)
+
+# It will print from index: 1 to 7
+print(name[1:8]) 
+
+# It will print the whole string by skipping 1 letter
+print(name[::2])
+
+# It will print name in reverse
+print(name[-1:-13:-1])   
+
+# It will also print in reverse
+print(name[12::-1])     
