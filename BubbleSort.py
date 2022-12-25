6fc10ef8548b5d916c7bf21a52f9ce0b2bee471a
Dipesh Yadav <dipeshyadav4444@gmail.com>
2022-12-25 08:36:09 +0530

    BubbleSort added

---
+++ BubbleSort.py
@@ -0,0 +1,20 @@
+def bubbleSort(lst):
+    length = len(lst)
+    for i in range(length):
+        for j in range(1,length-i):
+            if lst[j] < lst[j-1]:
+                temp = lst[j]
+                lst[j] = lst[j-1]
+                lst[j-1] = temp
+
+                # It can also be used for swapping
+                # Easy way of swapping
+                # (list[j],list[j-1]) = (list[j-1],list[j])   
+    return lst
+
+    
+lst = []
+for _ in range(int(input("Enter length: "))):
+    lst.append(int(input("Enter num: ")))
+print(lst)
+print(bubbleSort(lst))
